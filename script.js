6ef249a52c4bf2c4321020f88313eda7f94c291a
Noe Motamedi <noemotamedi@students.berkeley.net>
2018-05-17 10:33:41 -0700

    BITCONNEEEEEEEEEEEEEEEEEECT

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 
 var userInformation={};
 
+
+function SignUpUser(){
+    $.ajax({
+        type: 'POST',
+        contentType: 'application/json',
+        data: {
+            "email": $('#signUpEmail').val(),
+            "password": $('#signUpPassword').val()
+        },
+        dataType: 'json',
+        success: function(data){
+            runMySuccessFunction(data);
+        },
+        error: function(){
+            alert("failed");
+        },
+        url: 'https://slkidsbackend.herokuapp.com/[appName]/api/users'
+    });
+
+}
+
+
+
+
 function LoginUser(){
 
     var emailToGet =  $('#loginEmail').val();
@@ -18,3 +42,4 @@ function LoginUser(){
     });
 
 }
+
