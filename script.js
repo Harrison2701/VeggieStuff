3e062148e654a5cd67a205c6d0983aea2a59ccc3
Noe Motamedi <noemotamedi@students.berkeley.net>
2018-05-17 10:31:11 -0700

    BITCONNEEEEEEEEEEEEEEEEEECT

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-$.ajax({
-    url:"https://slkidsbackend.herokvapp.com/[appname]/api/users",
-    type: GET/POST,
-    dataType: "json",
-    success: function(){
-        //stuff
-        return;
-    },
-        data: userInformation,
-});
 
 var userInformation={};
 
+function LoginUser(){
+
+    var emailToGet =  $('#loginEmail').val();
+
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        success: function(emailToGet,passwordToGet){
+            runMySuccessFunction(data);
+        },
+        error: function(){
+            alert("failed");
+        },
+        url: 'https://slkidsbackend.herokuapp.com/VeggieGang/api/users/' + emailToGet
+    });
+
+}
