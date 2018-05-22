ce7f053c1be7aff4044da844b4b7ad7f144affe0
Noe Motamedi <noemotamedi@students.berkeley.net>
2018-05-22 10:22:24 -0700

    BITCONNEEEEEEEEEEEEEEEEEECT

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
                     }),
                     dataType: 'json',
                     success: function (data) {
-                        runMySuccessFunction(data);
+                        return;
                     },
                     error: function () {
                         alert("failed");
