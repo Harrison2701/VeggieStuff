5e1e8e83a37beff10b1345db4cbc95139ac29fb0
petercortez00 <christophercortez1@students.berkeley.net>
2018-05-25 10:10:17 -0700

    BITCONNECT

--- script.js
+++ script.js
@@ -53,6 +53,15 @@ $(document).ready(function() {
 
 });
 
+function myFunction() {
+    var x = document.getElementById("loginPassword");
+    if (x.type == "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
 
 
 
