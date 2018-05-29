40c8abe347a12843b3711f2ee21fcc5ebb4f3c00
Harrison Nguyen <harrisonnguyen@students.berkeley.net>
2018-05-29 10:23:21 -0700

    HI

--- harrison.js
+++ harrison.js
@@ -1,6 +1,11 @@
+
 function addMeat(){
     var meat = document.getElementById("selectBox1").value;
     var quantity = document.getElementById("selectBox2").value;
     var amount = "oz"
     document.getElementById("listMeat").innerHTML += '<p>' + meat + " " + quantity + amount + '</p>'
+}
+
+function calculateMeat(){
+
 }
