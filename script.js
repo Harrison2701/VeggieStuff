7d2882179a87e9806a228e019c66de53d55f131b
petercortez00 <christophercortez1@students.berkeley.net>
2018-05-30 10:07:37 -0700

    BITCONNECT

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 function addMeat(){
     var meat = document.getElementById("selectBox1").value;
     var quantity = document.getElementById("selectBox2").value;
-    var amount = "oz"
+    var amount = "oz";
     document.getElementById("listMeat").innerHTML += '<p>' + meat + " " + quantity + amount + '</p>'
 }
 
