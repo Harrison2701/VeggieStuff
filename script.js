30fba5561a48a9c9f2c5009f4c525925ea9dc3d4
Harrison Nguyen <harrisonnguyen@students.berkeley.net>
2018-05-31 10:17:24 -0700

    HI

--- script.js
+++ script.js
@@ -78,9 +78,43 @@ $(document).ready(function() {
 
 function addMeat(){
     var meat = document.getElementById("selectBox1").value;
-    var quantity = document.getElementById("selectBox2").value;
-    var amount = "oz"
-    document.getElementById("listMeat").innerHTML += '<p>' + meat + " " + quantity + amount + '</p>'
+    var amount = document.getElementById("selectBox2").value;
+    var oz = "oz"
+    document.getElementById("listMeat").innerHTML += '<p>' + meat + " " + amount + oz + '</p>'
+    var amounts = parseInt(amount)
+    if(meat == "Pork"){
+        meatsPork.push({"meat":meat,"total":amounts})
+    }
+    if(meat == "Poultry"){
+        meatsPoultry.push({"meat":meat,"total":amounts})
+    }
+    if(meat == "Beef"){
+        meatsBeef.push({"meat":meat,"total":amounts})
+    }
+}
+
+var meatsPork = []
+var meatsPoultry = []
+var meatsBeef = []
+
+function calculateMeat(){
+    var poultry = 0;
+    var beef = 0;
+    var pork = 0;
+
+    for(var i=0;i<meatsPork.length;i++){
+        pork += meatsPork[i].total
+    }
+    for(var j=0;j<meatsPoultry.length;j++){
+        poultry += meatsPoultry[j].total
+    }
+    for(var k=0;k<meatsBeef.length;k++){
+        beef += meatsBeef[k].total
+    }
+
+    console.log(pork)
+    console.log(poultry)
+    console.log(beef)
 }
 
 
