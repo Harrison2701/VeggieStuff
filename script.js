ef303b3076a81e4ec5cae43a6c3584e99aaa991f
petercortez00 <christophercortez1@students.berkeley.net>
2018-06-01 09:43:41 -0700

    he

--- script.js
+++ script.js
@@ -122,13 +122,13 @@ function calculateMeat(){
         beef += meatsBeef[k].total
     }
 
-    var poultryLB = poultry/16
-    var beefLB = beef/16
-    var porkLB = pork/16
+    var poultryLB = poultry/16;
+    var beefLB = beef/16;
+    var porkLB = pork/16;
 
-    console.log(pork)
-    console.log(poultry)
-    console.log(beef)
+    console.log(pork);
+    console.log(poultry);
+    console.log(beef);
 
     console.log(porkLB)
 }
